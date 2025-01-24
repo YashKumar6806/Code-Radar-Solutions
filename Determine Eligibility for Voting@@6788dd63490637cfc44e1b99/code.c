09cb18714e13e1126944b99d777d69cde2fd7769
YashKumar6806 <yash0160.becse24@chitkara.edu.in>
2025-01-24 12:50:10 +0530

    Solution to Determine Eligibility for Voting

--- Determine Eligibility for Voting@@6788dd63490637cfc44e1b99/code.c
+++ Determine Eligibility for Voting@@6788dd63490637cfc44e1b99/code.c
@@ -5,6 +5,8 @@ int main(){
     int n;
     scanf("%d",&n);
     if(a>=18)printf("Eligible");
-    else printf("Not Eligible");
+    else {
+        printf("Not Eligible");
+        }
     return 0;
 }
