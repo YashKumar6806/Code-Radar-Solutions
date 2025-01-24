5eb6bf1b12de71ef699c9157ae4378391b4514e2
YashKumar6806 <yash0160.becse24@chitkara.edu.in>
2025-01-24 14:33:35 +0530

    Solution to Check Number Sign

--- Check Number Sign@@6788dd63490637cfc44e1b9c/code.c
+++ Check Number Sign@@6788dd63490637cfc44e1b9c/code.c
@@ -6,7 +6,7 @@ int main() {
     int n;
     scanf("%d",&n);
     if(n>0)printf("Postive");
-    else if(n==0)printf("Negative");
-    else printf("Zero");
+    else if(n==0)printf("Zero");
+    else printf("Negative");
     return 0;
 }
