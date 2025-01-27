320438d4d9fe506668270ee87c18d05774fe3a22
YashKumar6806 <yash0160.becse24@chitkara.edu.in>
2025-01-27 14:21:46 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+
+int main() {
+    int n1,n2;
+    char ch;
+    scanf("%d",&n1);
+    scanf("%d",&n2);
+    scanf("%s",&ch);
+
+    int sum = n1 ch n2;
+    printf("%d",sum);   
+}
