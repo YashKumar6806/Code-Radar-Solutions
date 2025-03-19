390ee0621da71f06cfcafece360c45b4aee87cd9
YashKumar6806 <yash0160.becse24@chitkara.edu.in>
2025-03-19 14:50:43 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,42 +1,19 @@
 // Your code here...
 
-#include <stdio.h>
-
-// Bubble sort function
-void bubbleSort(int a[], int b) {
-    for (int i = 0; i < b - 1; i++) {
-        for (int j = 0; j < b - i - 1; j++) {
-            if (a[j] > a[j + 1]) {
-                // Swap the elements if they are in the wrong order
-                int temp = a[j];
-                a[j] = a[j + 1];
-                a[j + 1] = temp;
-            }
+int bubbleSort(int arr[],int n){
+    int max = arr[0];
+    for(int i=0;i<n;i++){
+        if(arr[i]>max){
+            max = arr[i];
         }
     }
+    return max;
+
 }
 
-// Function to print the array
-void printArray(int a[], int b) {
-    for (int i = 0; i < b; i++) {
-        printf("%d ", a[i]);
+int printArray(int arr,int n){
+    for(int i=0;i<n;i++){
+        printf("%d",arr[i]);
+        printf(" ");
     }
-    printf("\n"); // Add newline at the end of the output
 }
-
-int main() {
-    int arr[] = {64, 25, 12, 22, 11};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    
-    printf("Original array: ");
-    printArray(arr, n);
-
-    bubbleSort(arr, n);
-
-    printf("Sorted array: ");
-    printArray(arr, n);
-
-    return 0;
-}
-
-
