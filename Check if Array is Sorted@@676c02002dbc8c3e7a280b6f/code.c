4cc7d561e8ad0b1df3d8dd7c46cbd1e9fd8312bd
YashKumar6806 <yash0160.becse24@chitkara.edu.in>
2025-03-27 14:42:19 +0530

    Solution to Check if Array is Sorted

--- Check if Array is Sorted@@676c02002dbc8c3e7a280b6f/code.c
+++ Check if Array is Sorted@@676c02002dbc8c3e7a280b6f/code.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 
+#include <stdio.h>
+
 int main() {
     int n;
-
+    printf("Enter length for array: ");
     scanf("%d", &n);
 
     int a[n];
     int temp;
 
-
-
+    // Input elements into the array
+    printf("Enter elements of the array: \n");
     for (int i = 0; i < n; i++) {
         scanf("%d", &a[i]);
     }
 
-
+    // Bubble sort to sort the array in ascending order
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (a[j] > a[j + 1]) {
-
+                // Swap the elements
                 temp = a[j];
                 a[j] = a[j + 1];
                 a[j + 1] = temp;
@@ -26,29 +28,91 @@ int main() {
         }
     }
 
-  
-    int isSorted = 1; 
+    // Check if the array is sorted in ascending order
+    int isSortedAscending = 1;
     for (int i = 0; i < n - 1; i++) {
         if (a[i] > a[i + 1]) {
-            isSorted = 0; 
+            isSortedAscending = 0;
             break;
         }
     }
 
-
-    if (isSorted) {
-        printf("Sorted\n");
-    } else {
-        printf("Not Sorted\n");
+    // Check if the array is sorted in descending order
+    int isSortedDescending = 1;
+    for (int i = 0; i < n - 1; i++) {
+        if (a[i] < a[i + 1]) {
+            isSortedDescending = 0;
+            break;
+        }
     }
 
+    // Output result based on the sorting order
+    if (isSortedDescending) {
+        printf("Not Sorted\n");  // If the array is sorted in descending order
+    } else if (isSortedAscending) {
+        printf("Sorted\n");  // If the array is sorted in ascending order
+    } else {
+        printf("Not Sorted\n");  // If the array is not sorted
+    }
 
-   // printf("Sorted array: \n");
-    //for (int i = 0; i < n; i++) {
-      //  printf("%d ", a[i]);
-    //}
-    //printf("\n")
+    // Optional: print the sorted array
+    printf("Sorted array: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
 
     return 0;
 }
+
+
+
+ 
+
+
+ 
+
+
+
+
+
+
+
+
+
+  
+  
+
+
+     
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
