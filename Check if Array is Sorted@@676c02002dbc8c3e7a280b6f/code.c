0eb06ff56da258f0ad4f2fac643e9e570f48d1c2
YashKumar6806 <yash0160.becse24@chitkara.edu.in>
2025-03-27 14:31:48 +0530

    Solution to Check if Array is Sorted

--- Check if Array is Sorted@@676c02002dbc8c3e7a280b6f/code.c
+++ Check if Array is Sorted@@676c02002dbc8c3e7a280b6f/code.c
@@ -1,30 +1,84 @@
 // Your code here...
 #include<stdio.h>
-int main(){
+
+
+int main() {
     int n;
-    printf("Enter length for array:");
-    scanf("%d",&n);
+    printf("Enter length for array: ");
+    scanf("%d", &n);
+    
     int a[n];
     int temp;
-    for(int i=0;i<n;i++){
-        scanf("%d",&a[i]);
+    
+
+    printf("Enter elements of the array: \n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
     }
-    for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(a[i]>a[j]){
-                temp = a[i];
-                a[i]=a[j];
-                a[j]=temp;
-                printf("Sorted");
-            else {
-                printf("Not Sorted");
+    
 
-            }
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
 
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
             }
         }
     }
 
+  
+    int i
+    for (int i = 0; i < n - 1; i++) {
+        if (a[i] > a[i + 1]) {
+            isSorted = 0; 
+            break;
+        }
+    }
+
+
+    if (isSorted) {
+        printf("Sorted\n");
+    } else {
+        printf("Not Sorted\n");
+    }
+    
+
+    printf("Sorted array: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+
+
+ 
+
+
+
+
+
+
+
+
+
+      
+
+       
+     
+     
+
+
+
+
+
+
+
+
+
 
    
-}
+
